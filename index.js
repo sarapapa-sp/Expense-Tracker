a0ff317c94b4940695c335b70fa4af1393556895
sarapapa-sp <shubhampawar1705@gmail.com>
2022-10-01 16:43:22 +0530

    changes

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 
-import expenseentryRouter from "./Routes/expenseentry.js"
+import expenseentryRouter from "./Routes/expenseentry.js";
 
-const app = express()
+const app = express();
 const connect = async () => {
   try {
     mongoose.connect(
-      "mongodb+srv://sarapapa:DJ5wb0mxkbuLwlgj@cluster0.wj4airo.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://sarapapa:<Password>@cluster0.wj4airo.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("connected");
   } catch (err) {
     throw err;
   }
 };
-// Check is connection is lost 
+// Check is connection is lost
 mongoose.connection.on("disconnected", () => {
   console.log("Disconnected");
 });
 
 // adding the routes
-app.use(express.json())
-app.use("/api",expenseentryRouter)
+app.use(express.json());
+app.use("/api", expenseentryRouter);
 app.use((err, req, res, next) => {
   const errorStatus = err.status;
   const message = err.message;
@@ -39,5 +39,3 @@ app.listen(8080, () => {
   connect();
   console.log("Connected to the backend ");
 });
-
-
